d4af656ee91ab832b64c8a9133fcffae1f7b4c94
jtong <tj19832@gmail.com>
2014-03-03 23:17:55 +0800

    jtong - integrate ruby based  web project , depend on jt-mobile-kit 1.2.1b

--- lib/wdk/wdk_cli.rb
+++ lib/wdk/wdk_cli.rb
@@ -6,7 +6,12 @@ require "jt-mobile-kit/www_rb/www_rb"
 
 class WDK_Cli < Thor
   register JtGenerator, :g, "g", "Generate something"
-  register WwwRb, :new, "new", "create a new application"
+  register WwwRb, :ruby_based, "ruby_scaffold", "create a new application"
+
+  desc :new, "new ruby based web project"
+  def new project_name
+    WwwRb.start [:new, project_name]
+  end
 end
 
 
